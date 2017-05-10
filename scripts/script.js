02071b4ffdd2ed082eda32c4efcae5195e459e79
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-05-10 11:31:34 -0600

    Comleted site, need to add about food pairing page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   const foodIds = JSON.parse(localStorage.getItem('foodIds'));
+
   listChoice();
   listChoicebeerOptions();
   listChoiceWineOptions();
@@ -7,6 +8,7 @@ $(document).ready(function() {
   ingredientSelection();
   recipeSearch();
   recipeModals();
+
   function listChoice() {
     $('.drink').change(function() {
       const $options = $('.beerAndWine').children().hide();
@@ -16,6 +18,7 @@ $(document).ready(function() {
       }
     }).trigger('change');
   }
+
   function listChoicebeerOptions() {
     $('.beer').change(function() {
       const $options = $('.drinkPair').children().hide();
@@ -25,6 +28,7 @@ $(document).ready(function() {
       }
     }).trigger('change');
   }
+
   function listChoiceWineOptions() {
     $('.wine').change(function() {
       const $options = $('.drinkPair').children().hide();
@@ -44,16 +48,18 @@ $(document).ready(function() {
       const type = $('#drinkChoice').val();
       const drink = $('.beerSelect').val() || $('.wineSelect').val();
       const food = $('.' + drink).val();
-      // const id = beerFoodId[drink][food];
       const id = foodIds[type][drink][food];
       $.getJSON('https://g-foodpairing.herokuapp.com/ingredients/' + id + '/pairings?order=random', data => createIngredients(data, food));
     });
   }
+
   function createIngredients(data, food) {
     $('#break1').html('');
     $('#break1').append('<br><br><br><br><br><br><br><br>');
+    $('#ingredientInstruction').after('<br><br>');
     $('#pairIngredientsDiv').addClass('navBarFix');
     $('#ingredientInstruction').removeClass('hidden');
+
     for (let i = 0; i < data.length; i++) {
       const $cardHead = $('<div>');
       const $ingredientCard = $('<div class="card text-center ingredientCard">');
@@ -65,12 +71,12 @@ $(document).ready(function() {
 
       let ingredientName = data[i]._links.ingredient.name;
       if (ingredientName.includes("'")) {
-          ingredientName = ingredientName.slice(0, (ingredientName.indexOf("'") - 1));
-        } else if (ingredientName.includes('(')) {
-          ingredientName = ingredientName.slice(0, (ingredientName.indexOf('(') - 1));
-        } else if (ingredientName.includes('®')) {
-          ingredientName = ingredientName.slice(0, ingredientName.length - 1);
-        }
+        ingredientName = ingredientName.slice(0, (ingredientName.indexOf("'") - 1));
+      } else if (ingredientName.includes('(')) {
+        ingredientName = ingredientName.slice(0, (ingredientName.indexOf('(') - 1));
+      } else if (ingredientName.includes('®')) {
+        ingredientName = ingredientName.slice(0, ingredientName.length - 1);
+      }
       const $cardTitle = $('<div class="card-header">' + ingredientName + '</div>');
 
       $cardImageBox.append($cardImage);
@@ -97,25 +103,25 @@ $(document).ready(function() {
       const classes2 = 'card text-center ingredientCard card-inverse card-success';
       let choice;
       if ($twoParents.attr('class') === classes1) {
-          $twoParents.toggleClass('card-inverse card-success');
-          choice = $twoParents.children()[0].innerText;
-        } else if ($twoParents.attr('class') === classes2) {
-          $twoParents.toggleClass('card-inverse card-success');
-          choice = $twoParents.children()[0].innerText;
-        } else if ($oneParents.attr('class') === classes2) {
-          $oneParents.toggleClass('card-inverse card-success');
-          choice = $oneParents.children()[0].innerText;
-        } else if ($oneParents.attr('class') === classes1) {
-          $oneParents.toggleClass('card-inverse card-success');
-          choice = $oneParents.children()[0].innerText;
-        }
+        $twoParents.toggleClass('card-inverse card-success');
+        choice = $twoParents.children()[0].innerText;
+      } else if ($twoParents.attr('class') === classes2) {
+        $twoParents.toggleClass('card-inverse card-success');
+        choice = $twoParents.children()[0].innerText;
+      } else if ($oneParents.attr('class') === classes2) {
+        $oneParents.toggleClass('card-inverse card-success');
+        choice = $oneParents.children()[0].innerText;
+      } else if ($oneParents.attr('class') === classes1) {
+        $oneParents.toggleClass('card-inverse card-success');
+        choice = $oneParents.children()[0].innerText;
+      }
       if (ingredient.val().includes(choice.trim()) && ingredient.val().indexOf(choice.trim() !== 0)) {
-          ingredient.val(ingredient.val().split(', ').filter(element => element !== choice.trim()).join(', '));
-        } else if (ingredient.val()) {
-          ingredient.val(ingredient.val() + ', ' + choice);
-        } else {
-          ingredient.val(choice);
-        }
+        ingredient.val(ingredient.val().split(', ').filter(element => element !== choice.trim()).join(', '));
+      } else if (ingredient.val()) {
+        ingredient.val(ingredient.val() + ', ' + choice);
+      } else {
+        ingredient.val(choice);
+      }
     });
 
   }
@@ -132,6 +138,7 @@ $(document).ready(function() {
       $.getJSON('https://g-food2fork.herokuapp.com/api/search?&q=' + search2, data => createRecipes2(data));
     });
   }
+
   function createRecipes1(data) {
     $('#break2').html('');
     $('#break2').append('<br><br><br><br><br><br><br><br>');
@@ -147,37 +154,39 @@ $(document).ready(function() {
     $('footer').removeClass('hidden');
     $('.searchMore').removeClass('hidden');
   }
+
   function recipeInfo1(recipeData) {
     const $card = $('<div class="card recipeCard">');
     const image = recipeData.images[0].hostedLargeUrl;
-    const $image = $('<img class="card-img-top center-text" src="' + image + '">');
+    const $image = $('<img class="card-img-top center-text" src="' + image + '" onerror="this.src=\'images/missing.jpg\'">');
     const $cardBlock = $('<div class="card-block d-flex p-2 flex-column justify-content-between">');
     const recipeName = recipeData.name;
     const $cardTitle = $('<h4 class="card-text">' + recipeName + '</h4><br>');
-    const link = recipeData.source.sourceRecipeUrl;
+    // const link = recipeData.source.sourceRecipeUrl;
+    const link = recipeData.attribution.url;
     const $link = $('<a class="btn btn-success" href="' + link + '" target="_blank" data-toggle="modal" data-target="#recipePopUp">Go to Recipe</button>');
-    //const link = recipeData.attribution.url;
     $cardBlock.append($cardTitle);
     $cardBlock.append($link);
     $card.append($image);
     $card.append($cardBlock);
     $('#recipeList1').append($card);
   }
+
   function createRecipes2(data) {
     $('#break3').append('<br>');
     for (let i = 0; i < data.recipes.length; i++) {
       recipeInfo2(data, i);
     }
   }
+
   function recipeInfo2(data, i) {
     const $card = $('<div class="card recipeCard">');
     const image = data.recipes[i].image_url;
-    const $image = $('<img class="card-img-top center-text" src="' + image + '">');
+    const $image = $('<img class="card-img-top center-text" src="' + image + '" onerror="this.src=\'images/missing.jpg\'">');
     const $cardBlock = $('<div class="card-block d-flex p-2 flex-column justify-content-between">');
     const recipeName = data.recipes[i].title;
     const $cardTitle = $('<h4 class="card-text">' + recipeName + '</h4>');
     const link = data.recipes[i].f2f_url;
-    // const link = data.recipes[i].source_url;
     const $link = $('<a class="btn btn-success" href="' + link + '" target="_blank" data-toggle="modal" data-target="#recipePopUp">Go to Recipe</button>');
     $cardBlock.append($cardTitle);
     $cardBlock.append($link);
@@ -198,193 +207,9 @@ $(document).ready(function() {
       }
     });
   }
+
   function createModal(event) {
     const url = $(event.target).prop('href');
     $('#recipeIframe').attr('src', url);
   }
-
-  function replaceImages() {
-    $('img').error(function(){
-        $(this).attr('src', 'missing.jpg');
-    });
-  }
-  // /"https://static.yummly.co/api-logo.png" yummly logo
-  //
-  // const beerFoodId = {
-  //   crisp: {
-  //     'Brown Rice': 4413,
-  //     'Quinoa': 5835,
-  //     'Basmati Rice': 4309
-  //   },
-  //   maltyFruity: {
-  //     'Fava Beans': 4652,
-  //     'Chickpeas': 3950,
-  //     'Green Beans': 4715,
-  //     'Clams': 4539,
-  //     'Scallops': 5192,
-  //     'Lobster': 4863,
-  //     'Shrimp': 5222
-  //   },
-  //   sour: {
-  //     'Beef': 3068,
-  //     'Lamb': 4823,
-  //     'Parsnips': 5022,
-  //     'Carrots': 137,
-  //     'Beets': 135
-  //   },
-  //   hoppyBitter: {
-  //     'Duck': 5518,
-  //     'Quail': 5520,
-  //     'Chicken': 4517,
-  //     'Brown Rice': 4413,
-  //     'Quinoa': 5835,
-  //     'Basmati Rice': 4309,
-  //     'Butter': 1376,
-  //     'Olive Oil': 1408,
-  //     'Sausage': 3351,
-  //     'Bacon': 4290,
-  //     'Pork Loin': 5093
-  //   },
-  //   dark: {
-  //     'Butter': 1376,
-  //     'Olive Oil': 1408,
-  //     'Carrots': 137,
-  //     'Bell Peppers': 136,
-  //     'Mushrooms': 4948,
-  //     'Broccoli': 4411,
-  //     'Dark Chocolate': 1518,
-  //     'Milk Chocolate': 1544
-  //   },
-  //   belgian: {
-  //     'Brie': 1910,
-  //     'Cheddar': 1727,
-  //     'Blue Cheese': 1751,
-  //     'Mozzarella': 4935,
-  //     'Goat Cheese': 4697
-  //   },
-  //   maltySweet: {
-  //     'Dark Chocolate': 1518,
-  //     'Milk Chocolate': 1544
-  //   },
-  //   fruitySweet: {
-  //     'Sausage': 3351,
-  //     'Bacon': 4290,
-  //     'Pork Loin': 5093
-  //   },
-  //   sweet: {
-  //     'Ice Cream': 191,
-  //     'Cake': 1104
-  //   }
-  // };
-  //
-  // const foodIds = {
-  //   beer: {
-  //     crisp: {
-  //       'Brown Rice': 4413,
-  //       'Quinoa': 5835,
-  //       'Basmati Rice': 4309
-  //     },
-  //     maltyFruity: {
-  //       'Fava Beans': 4652,
-  //       'Chickpeas': 3950,
-  //       'Green Beans': 4715,
-  //       'Clams': 4539,
-  //       'Scallops': 5192,
-  //       'Lobster': 4863,
-  //       'Shrimp': 5222
-  //     },
-  //     sour: {
-  //       'Beef': 3068,
-  //       'Lamb': 4823,
-  //       'Parsnips': 5022,
-  //       'Carrots': 137,
-  //       'Beets': 135
-  //     },
-  //     hoppyBitter: {
-  //       'Duck': 5518,
-  //       'Quail': 5520,
-  //       'Chicken': 4517,
-  //       'Brown Rice': 4413,
-  //       'Quinoa': 5835,
-  //       'Basmati Rice': 4309,
-  //       'Butter': 1376,
-  //       'Olive Oil': 1408,
-  //       'Sausage': 3351,
-  //       'Bacon': 4290,
-  //       'Pork Loin': 5093
-  //     },
-  //     dark: {
-  //       'Butter': 1376,
-  //       'Olive Oil': 1408,
-  //       'Carrots': 137,
-  //       'Bell Peppers': 136,
-  //       'Mushrooms': 4948,
-  //       'Broccoli': 4411,
-  //       'Dark Chocolate': 1518,
-  //       'Milk Chocolate': 1544
-  //     },
-  //     belgian: {
-  //       'Brie': 1910,
-  //       'Cheddar': 1727,
-  //       'Blue Cheese': 1751,
-  //       'Mozzarella': 4935,
-  //       'Goat Cheese': 4697
-  //     },
-  //     maltySweet: {
-  //       'Dark Chocolate': 1518,
-  //       'Milk Chocolate': 1544
-  //     },
-  //     fruitySweet: {
-  //       'Sausage': 3351,
-  //       'Bacon': 4290,
-  //       'Pork Loin': 5093
-  //     },
-  //     sweet: {
-  //       'Ice Cream': 191,
-  //       'Cake': 1104
-  //     }
-  //   },
-  //   wine: {
-  //     dryWhite: {
-  //       'Goat Cheese': 4697,
-  //       'Chicken': 4517,
-  //       'Turkey': 5350,
-  //       'Shrimp': 5222,
-  //       'Asparagus': 5777
-  //     },
-  //     sweetWhite: {
-  //       'Duck': 5709,
-  //       'Sea Bass': 5844,
-  //       'Trout': 2042,
-  //     },
-  //     richWhite: {
-  //       'Blue Cheese': 1751,
-  //       'Veal': 5523,
-  //       'Crab': 2089,
-  //       'Squash': 4003
-  //     },
-  //     lightRed: {
-  //       'Goat Cheese': 4697,
-  //       'Chicken': 4517,
-  //       'Lamb': 4823,
-  //       'Mushrooms': 4948
-  //     },
-  //     mediumRed: {
-  //       'Cheddar': 1727,
-  //       'Salmon': 4282,
-  //       'Pork Belly': 5848,
-  //       'Pork Loin': 5093,
-  //       'Beets': 135
-  //     },
-  //     boldRed: {
-  //       'Parmesan': 1794,
-  //       'Beef': 3068,
-  //       'Ribeye': 3068,
-  //       'Tomatoes': 5333
-  //     }
-  //   }
-  // };
-  //
-
-
 });
